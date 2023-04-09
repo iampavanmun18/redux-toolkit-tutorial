51a70e10ae85a6af443d50c93471b3535eae55d1
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-04-09 11:19:10 +0530

    added Modal for clear all (Button) and createde slice for modal reducer

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,9 +1,25 @@
+import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
-
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { calculateCart } from "./features/cart/cartSlice";
+import ModalSlice from "./features/modalSlice";
+import Modal from "./components/Modal";
 function App() {
+  const { cartItems } = useSelector((store) => store.cart)
+  const {isOpen} = useSelector((store) => store.modal)
+  const dispatch  = useDispatch();
+
+  console.log("isMOdal", isOpen)
+
+  useEffect(()=>{
+    dispatch(calculateCart())
+  },[cartItems])
   return (
     <main>
+      {isOpen && <Modal/>}
       <Navbar />
+      <CartContainer />
     </main>
   )
     ;
