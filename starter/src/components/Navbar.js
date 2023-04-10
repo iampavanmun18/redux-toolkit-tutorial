1d3544d3c4f0d10d9d29b077ab24ed9d23e0ebd8
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-04-10 12:44:41 +0530

    added Barlatest icon from heroicon

--- starter/src/components/Navbar.js
+++ starter/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Bar, CartIcon } from '../icons'
+import { Bar, BarLatest, CartIcon } from '../icons'
 const Navbar = () => {
 
     //we can destructure amount from  store(whole state of the app )
@@ -11,8 +11,9 @@ const Navbar = () => {
             <div className="nav-center">
                 <h3>Redux Toolkit</h3>
                 <div className="nav-container">
-                    <Bar/>
+                    <Bar />
                     <CartIcon />
+                    <BarLatest />
                     <div className="amount-container">
                         <p className='total-amount'>{amount}</p>
                     </div>
--- starter/src/icons.js
+++ starter/src/icons.js
@@ -58,3 +58,13 @@ export const ChevronUp = () => {
     </svg>
   );
 };
+
+
+export const BarLatest = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+    </svg>
+
+  )
+}
